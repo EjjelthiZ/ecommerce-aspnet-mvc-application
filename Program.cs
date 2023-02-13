82671389dbce13b9d462b0c71b9079392f102003
Zakariae <zejjelthi@mghconseil.fr>
2023-02-13 15:19:15 +0100

    Basic Views Completed

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -23,5 +27,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//Seed Database
+AppDbInitializer.Seed(app);
 
 app.Run();
